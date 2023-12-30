d8779cdbc225b916c1589848b5e9ea22da149636
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-30 14:36:01 -0300

    feat: created text validation methods

--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -7,6 +7,24 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesString
     {
-       
+        public int RetornarQuantidadeCaracteres(string texto)
+        {
+            int quantidadeDeCaracteres = texto.Length;
+            return quantidadeDeCaracteres;
+        }
+
+        public bool ContemCaractere(string texto, string palavra)
+        {
+            bool contemPalavra = texto.Contains(palavra, StringComparison.OrdinalIgnoreCase);
+
+            return contemPalavra;
+        }
+
+        public bool TextoTerminaCom(string texto, string palavra)
+        {
+            bool contemPalavra = texto.EndsWith(palavra, StringComparison.OrdinalIgnoreCase);
+
+            return contemPalavra;
+        }
     }
 }
