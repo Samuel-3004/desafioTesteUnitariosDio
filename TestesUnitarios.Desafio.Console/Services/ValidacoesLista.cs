5d533d3a43a9309b5c7323da9020b0f844d2a6a0
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-30 14:34:33 -0300

    feat: created list validation methods

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -7,6 +7,33 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesLista
     {
-       
+        public List<int> RemoverNumerosNegativos(List<int> numList)
+        {
+            List<int> numerosPositivos = numList.Where(numero => numero > 0).ToList();
+            return numerosPositivos;
+        }
+
+        public bool ListaContemDeterminadoNumero(List<int> numList, int num)
+        {
+            return numList.Contains(num);
+        }
+
+        public List<int> MultiplicarNumerosLista(List<int> numList, int num)
+        {
+            List<int> novaLista = numList.Select(numero => numero * num).ToList();
+            return novaLista;
+        }
+
+        public int RetornarMaiorNumeroLista(List<int> numList)
+        {
+            int maiorNumeroDaLista = numList.Max();
+            return maiorNumeroDaLista;
+        }
+
+        public int RetornarMenorNumeroLista(List<int> numList)
+        {
+            int menorNumeroDaLista = numList.Min();
+            return menorNumeroDaLista;
+        }
     }
 }
