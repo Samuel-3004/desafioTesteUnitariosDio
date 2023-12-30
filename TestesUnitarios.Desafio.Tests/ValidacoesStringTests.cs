4287007d8e080066e89707c70da3c58bf39647b7
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-30 14:38:30 -0300

    feat: testing of validation methods for created texts

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -8,6 +8,66 @@ namespace TestesUnitarios.Desafio.Tests
 {
     public class ValidacoesStringTests
     {
-       
+        private ValidacoesString _texto;
+
+        public ValidacoesStringTests()
+        {
+            _texto = new ValidacoesString();
+        }
+
+        [Fact]
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+        {
+            // Arrange
+            string textoDeTeste = "Matrix";
+
+            //Act
+            int resultado = _texto.RetornarQuantidadeCaracteres(textoDeTeste);
+
+            //Assert
+            Assert.Equal(6, resultado);
+        }
+
+        [Fact]
+        public void DeveContemAPalavraQualquerNoTexto()
+        {
+            //Arrange
+            string texto = "Esse é um texto qualquer";
+            string palavraBuscada = "qualquer";
+
+            //Act
+            bool resultado = _texto.ContemCaractere(texto, palavraBuscada);
+
+            //Assert
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void NaoDeveConterAPalavraTesteNoTexto()
+        {
+            //Arrange
+            string texto = "Esse é um texto qualquer";
+            string palavraBuscada = "teste";
+
+            //Act
+            bool resultado = _texto.ContemCaractere(texto, palavraBuscada);
+
+            //Assert
+            Assert.False(resultado);
+        }
+
+        [Fact]
+        public void TextoDeveTerminarComAPalavraProcurado()
+        {
+            //Arrange
+            string texto = "Começo, meio e fim do texto procurado";
+            string palavraBuscada = "procurado";
+
+            //Act
+            bool resultado = _texto.TextoTerminaCom(texto, palavraBuscada);
+
+            //Assert
+            Assert.True(resultado);
+        }
     }
 }
